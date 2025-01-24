55cdc21d168cc98dbae43457cfced05bb41ab893
BenilBen <114132461+BenilBen@users.noreply.github.com>
2025-01-24 17:12:55 +0530

    Your commit message here

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b4a81cb6da84b7658b984922fb7056920058a1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3268551229"
+    serviceWorkerVersion: "1750058959"
   }
 });
